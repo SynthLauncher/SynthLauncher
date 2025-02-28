c076cb095b0aab80665f35ea52e82f98b7c79d80
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-02-28 13:01:25 +0400

    I forgot to not touch main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,3 @@
 #include <iostream>
-#include "include/entities/arch.hh"
 
-int main() {
-  std::string str = "str";
-  Architecture::arch_from_string(str);
-
-  return 0; 
-}
+int main() { return 0; }
