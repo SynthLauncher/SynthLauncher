e5640301c179793d9606d502b991c14f350f0e84
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-05-08 18:59:50 +0400

    Added TailwindCSS

--- crates/sl-frontend/src/App.vue
+++ crates/sl-frontend/src/App.vue
@@ -1,162 +1,29 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
-
+import { appWindow } from "@tauri-apps/api/window"
 const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
   greetMsg.value = await invoke("greet", { name: name.value });
 }
+
+function minimize() {
+  appWindow.minimize();
+}
 </script>
 
 <template>
-  <main class="container">
-    <h1>SynthLauncher</h1>
-
-    <p>Work in progress</p>
-
-    <button type="button">Launch</button>
-
-    <select name="Versions" id="Versions" class="Version">
-      <option value="1.20.4">1.20.4</option>
-      <option value="1.19.4">1.19.4</option>
-      <option value="1.8.9">1.8.9</option>
-      <option value="1.7.10">1.7.10</option>
-    </select>
-
-    <p>{{ greetMsg }}</p>
-  </main>
+  <div class="flex flex-col h-screen overflow-hidden">
+    <div
+      class="h-10 w-full flex items-center justify-between bg-gray-800 text-shadow-white px-4 select-none"
+      style="-webkit-app-region: drag;"
+    >
+      <div class="font-bold text-white text-sm">SynthLauncher</div>
+      <div class="flex gap-2 text-white" style="-webkit-app-region: no-drag;">
+        <button @click="minimize" class="w-8 h-8 hover:bg-gray-700 rounded">-</button>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
-<style>
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-select.Version {
-  position: relative;
-  height: 40px;
-  width: 150px;
-  left: 758px;
-  top: 250px;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #000000;
-  background-color: #00ff22;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-  position: relative;
-  top: 290px;
-  width: 715px;
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-</style>
