dd24cc92a89b24044dd5b112d20d36acd3f40d18
safiworks <116169309+safiworks@users.noreply.github.com>
2025-05-28 03:29:25 +0300

    [sl-frontend tauri]: fixed and uncommented commands

--- crates/sl-frontend/src-tauri/src/commands.rs
+++ crates/sl-frontend/src-tauri/src/commands.rs
@@ -1,63 +1,71 @@
-// use sl_core::config::config::Config;
+use sl_core::config::config::Config;
+use sl_core::instance::{Instance, InstanceType};
+use sl_core::instances::Instances;
+use sl_utils::utils::errors::BackendError;
 
-// async fn get_global_config() -> Result<Config, std::io::Error> {
-//     Ok(Config::read_global()?)
-// }
+async fn get_global_config() -> Result<Config, std::io::Error> {
+    Ok(Config::read_global()?)
+}
 
-// #[tauri::command]
-// pub async fn get_username() -> Result<String, String> {
-//     let config = get_global_config().await.map_err(|e| e.to_string())?;
-//     let username = config
-//         .get("auth_player_name")
-//         .ok_or("Missing 'auth_player_name' in config")?
-//         .to_string();
-//     Ok(username)
-// }
+#[tauri::command]
+pub async fn get_username() -> Result<String, String> {
+    let config = get_global_config().await.map_err(|e| e.to_string())?;
+    let username = config
+        .get("auth_player_name")
+        .ok_or("Missing 'auth_player_name' in config")?
+        .to_string();
+    Ok(username)
+}
 
-// #[tauri::command]
-// pub async fn edit_username(username: &str) -> Result<(), String> {
-//     let mut config = get_global_config().await.map_err(|e| e.to_string())?;
-//     config
-//         .update_config_field("auth_player_name", username)
-//         .unwrap();
+#[tauri::command]
+pub async fn edit_username(username: &str) -> Result<(), String> {
+    let mut config = get_global_config().await.map_err(|e| e.to_string())?;
+    config
+        .update_config_field("auth_player_name", username)
+        .unwrap();
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// #[tauri::command]
-// pub async fn get_installations() -> Result<Installations, String> {
-//     let installations = Installations::load().map_err(|e| e.to_string())?;
+#[tauri::command]
+pub async fn get_installations() -> Result<Instances, String> {
+    let installations = Instances::load().map_err(|e| e.to_string())?;
 
-//     Ok(installations)
-// }
+    Ok(installations)
+}
 
-// #[tauri::command]
-// pub async fn create_installation(name: &str, version: &str) -> Result<(), String> {
-//     let mut instance = Installation::new(&name, &version).unwrap();
-//     Installations::add(&instance).unwrap();
-//     instance.install().await.unwrap();
+async fn create_installation_inner(name: String, version: String) -> Result<(), BackendError> {
+    let mut instance = Instance::new(&name, &version, InstanceType::Vanilla, None)?;
+    Instances::add(&instance)?;
+    instance.install().await?;
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// #[tauri::command]
-// pub async fn remove_installation(name: &str) -> Result<(), String> {
-//     Installations::remove(name).unwrap();
-//     Ok(())
-// }
+#[tauri::command]
+pub async fn create_installation(name: String, version: String) -> Result<(), String> {
+    let results = tokio::task::spawn_local(create_installation_inner(name, version));
+    results.await.unwrap().map_err(|e| e.to_string())
+}
 
-// #[tauri::command]
-// pub async fn load_all_installations() -> Result<(), String> {
-//     for instance in Installations::load_all_installations().unwrap().0 {
-//         Installations::add(&instance).unwrap();
-//     }
-//     Ok(())
-// }
+#[tauri::command]
+pub async fn remove_installation(name: &str) -> Result<(), String> {
+    Instances::remove(name).unwrap();
+    Ok(())
+}
 
-// #[tauri::command]
-// pub async fn launch(name: &str) -> Result<(), String> {
-//     let instance = Installations::find(name).map_err(|e| e.to_string())?;
-//     instance.execute(None).map_err(|e| e.to_string())?;
+#[tauri::command]
+pub async fn load_all_installations() -> Result<(), String> {
+    for instance in Instances::load_all_instances().unwrap().0 {
+        Instances::add(&instance).unwrap();
+    }
+    Ok(())
+}
 
-//     Ok(())
-// }
+#[tauri::command]
+pub async fn launch(name: &str) -> Result<(), String> {
+    let instance = Instances::find(name).map_err(|e| e.to_string())?;
+    instance.execute(None).await.map_err(|e| e.to_string())?;
+
+    Ok(())
+}
--- crates/sl-frontend/src-tauri/src/lib.rs
+++ crates/sl-frontend/src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 mod commands;
+use commands::{
+    create_installation, edit_username, get_installations, get_username, launch,
+    load_all_installations, remove_installation,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -6,13 +10,13 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            // launch,
-            // get_username,
-            // edit_username,
-            // get_installations,
-            // create_installation,
-            // remove_installation,
-            // load_all_installations
+            launch,
+            get_username,
+            edit_username,
+            get_installations,
+            create_installation,
+            remove_installation,
+            load_all_installations
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
