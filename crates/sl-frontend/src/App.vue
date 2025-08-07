583e1ab10cc7a6a8c87181c2bf09e18ed49e4eea
stierprogrammer <sabasajaia40@gmail.com>
2025-08-08 02:00:48 +0400

    Set up vue router

--- crates/sl-frontend/src/App.vue
+++ crates/sl-frontend/src/App.vue
@@ -1,24 +1,5 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
-
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
-</script>
-
 <template>
   <main>
-    <h1 class="text-red-500">Welcome to Tauri + Vue</h1>
-
-    <form @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+    <router-view />
   </main>
 </template>
--- crates/sl-frontend/src/main.ts
+++ crates/sl-frontend/src/main.ts
@@ -1,5 +1,17 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+import HomePage from './pages/HomePage.vue'
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import './App.css';
 
-createApp(App).mount("#app");
+const routes = [
+  { path: '/', component: HomePage },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount("#app");
