0aed6d588189ab30c53cc08548a87319690a923e
stierprogrammer <sabasajaia40@gmail.com>
2025-08-12 19:06:38 +0400

    [sl-frontend/components/AccountsSidebar.vue] Added 3D skin viewer

--- crates/sl-frontend/src/components/AccountsSidebar.vue
+++ crates/sl-frontend/src/components/AccountsSidebar.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import { SkinView3d } from 'vue-skinview3d';
+</script>
+
 <template>
     <main class="w-80 shrink-0 h-full border-neutral-700 border-t-2">
-        
+        <SkinView3d :width="300" :height="400"
+            skin-url="https://textures.minecraft.net/texture/ebff9a96d423d5d74c96905b0493a4c9b4e3a48cdffed145ab53a7c5487061d4"
+            :global-light="3" />
     </main>
 </template>
--- crates/sl-frontend/src/pages/HomePage.vue
+++ crates/sl-frontend/src/pages/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import StoreCard from '../components/StoreCard.vue';
-
 </script>
 
 <template>
     <main class="text-white">
-        <StoreCard icon="https://download.fo/icon.png" title="sigma" author="sigma" description="Beautiful graphics, speedy performance and familiar features in a simple package. 1.21.8 alpha!" :downloads="12" />
+
     </main>
 </template>
