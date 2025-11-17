50d87636c9b6a0c32e9fb75a1aef65af664e2fbe
safiworks <116169309+safiworks@users.noreply.github.com>
2025-11-17 07:56:03 +0200

    fix(sl-frontend main.ts): use createWebHistroy instead of createMemoryHistory

--- crates/sl-frontend/src/main.ts
+++ crates/sl-frontend/src/main.ts
@@ -1,22 +1,28 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
-import { createApp } from 'vue';
-import App from './App.vue';
-import './App.css';
+import { createRouter, createWebHistory } from "vue-router";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./App.css";
 
-import InstancesPage from './pages/InstancesPage.vue';
-import StorePage from './pages/StorePage.vue';
-import InstancePage from './pages/InstancePage.vue';
+import InstancesPage from "./pages/InstancesPage.vue";
+import StorePage from "./pages/StorePage.vue";
+import InstancePage from "./pages/InstancePage.vue";
+import ProgressPage from "./pages/ProgressPage.vue";
 
 const routes = [
-  { path: '/', name: 'instances', component: InstancesPage },
-  { path: '/store', name: 'store', component: StorePage },
-  { path: '/instance/:instance_name', name: 'instance', component: InstancePage },
+  { path: "/", name: "instances", component: InstancesPage },
+  { path: "/store", name: "store", component: StorePage },
+  {
+    path: "/instance/:instance_name",
+    name: "instance",
+    component: InstancePage,
+  },
+  { path: "/progress", name: "progress", component: ProgressPage },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
 const app = createApp(App);
-app.use(router).mount('#app')
+app.use(router).mount("#app");
